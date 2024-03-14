30cd291c2ba84ac345a6dcbd75ff9d783adf16fb
alwynmathew <alwynmathew.90@gmail.com>
2024-03-14 15:53:22 +0000

    Updated progress monitor to fit DTC ontology

--- progress_monitoring.py
+++ progress_monitoring.py
@@ -352,7 +352,8 @@ def parse_args():
     Get parameters from user
     """
     parser = argparse.ArgumentParser(description='Perform progress monitoring with DTP graph')
-    parser.add_argument('--xml_path', '-x', type=str, help='path to config xml file', default='DTP_API/DTP_config.xml')
+    parser.add_argument('--xml_path', '-x', type=str, help='path to config xml file',
+                        default='DTP_API_DTC/DTP_config.xml')
     parser.add_argument('--simulation', '-s', default=False, action='store_true')
 
     return parser.parse_args()
